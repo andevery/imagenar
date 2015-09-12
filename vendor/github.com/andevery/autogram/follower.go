506a719ac9f1a60b5a5f0570b30e56d35f7ddf89
andevery <andevery@gmail.com>
2015-09-12 07:30:48 +0300

    Refining follower

--- vendor/github.com/andevery/autogram/follower.go
+++ vendor/github.com/andevery/autogram/follower.go
@@ -2,15 +2,47 @@ package autogram
 
 import (
 	// "time"
-	"github.com/andevery/instaw"
+	"github.com/andevery/instax"
 )
 
 type Follower struct {
-	Limiter   *Limiter
-	WithLikes bool
-	Liker     *Liker
+	Limiter        *Limiter
+	WithLikes      bool
+	Liker          *Liker
+	UsersCondition struct {
+		MaxFollowedBy int
+		MinFollowedBy int
+		MaxFollows    int
+		MinFollows    int
+		MaxMedia      int
+	}
+
+	Insta *instax.Client
 }
 
-func (f *Follower) FollowBatch([]instaw.User) {
+func (f *Follower) FollowBatch(users []instax.User) {
+	for i, _ := range users {
 
+	}
+}
+
+func (f *Follower) isUserMathc(user *instax.User) bool {
+	flag := true
+	if f.UsersCondition.MaxFollowedBy > 0 {
+		flag = flag && user.Counts.FollowedBy <= f.UsersCondition.MaxFollowedBy
+	}
+	if f.UsersCondition.MinFollowedBy > 0 {
+		flag = flag && user.Counts.FollowedBy >= f.UsersCondition.MinFollowedBy
+	}
+	if f.UsersCondition.MaxFollows > 0 {
+		flag = flag && user.Counts.Follows <= f.UsersCondition.MaxFollows
+	}
+	if f.UsersCondition.MinFollows > 0 {
+		flag = flag && user.Counts.Follows >= f.UsersCondition.MinFollows
+	}
+	if f.UsersCondition.MaxMedia > 0 {
+		flag = flag && user.Counts.Media <= f.UsersCondition.MaxMedia
+	}
+
+	return flag
 }
